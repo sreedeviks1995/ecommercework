9cac3ad83e73933206fae9c765dbbd5fef61cef4
Sreedeviks09 <you@example.com>
2021-04-17 12:16:25 +0530

    addede

---
+++ ecommerce/ecommerceproject/shop/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
+    list_per_page = 20
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ['price', 'stock', 'available']
+
+
+admin.site.register(Product, ProductAdmin)
